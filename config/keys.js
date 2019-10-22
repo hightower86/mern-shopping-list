0b78fca641b5d3510264d0a951cf6936eae3688e
Adel <htwr86@gmail.com>
2019-10-22 22:30:36 +0300

    create express server and connect to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://adel:1q2w3e4r@mern-shopping-list-a8ptj.gcp.mongodb.net/shopping-list'
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//const bodyParser = require('express.body-parser');
+const app = express();
+
+//BodyParser middleware
+app.use(express.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongo
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected ...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5005;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
